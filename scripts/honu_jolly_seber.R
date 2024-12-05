d0cec5e31e4945aba88de2d125fe90c45b4ba485
skishimaru <shelbie.ishimaru@noaa.gov>
2024-12-04 16:17:29 -1000

    1st time getting the model to converge!

--- scripts/honu_jolly_seber.R
+++ scripts/honu_jolly_seber.R
@@ -1,19 +1,19 @@
 # Initial multi-state jolly-seber model for honu 
-# This model only includes nesting data collected from Tern Island in 2023
 # Created by: Shelbie Ishimaru
 # Created on: 2024-09-09
-# Last edited: 2024-11-29
+# Last edited: 2024-12-04
 ################################################################################
 # Preface ----------------------------------------------------------------------
 # This model is a learning tool for me to begin applying Bayesian analysis to real honu data
-# This model incorporates Tern Island night (nesting) survey data from the 2023 field season
-# This model is highly simplified and assumes 4 states (Pre Lalo (not yet nested), nesting, internesting interval, and post Lalo)
+# This model only includes Tern Island night (nesting) survey data from the 2023 field season
+# This model is highly simplified and assumes 4 states (Pre Lalo (not yet nested), Nesting, Internesting interval, and Post Lalo)
+# The code within this script is mainly adapted from chapter 10.3.2 & 10.4.2 of Bayesian Population Analysis using WinBUGS
 
 # Initialization ---------------------------------------------------------------
 library(R2jags) #to run JAGS
 library(shinystan) #to run shiny stan
 library(tidyverse) #to utilize pipe operators
-library(here) #to create unbreakable file paths
+library(here) #unbreakable file path to read in data
 
 # Constructing JAGS Model ------------------------------------------------------
 jags.js.ms.txt <- function(){  #CHANGED FROM BOOK SINK FUNCTION
@@ -53,23 +53,23 @@ jags.js.ms.txt <- function(){  #CHANGED FROM BOOK SINK FUNCTION
       ps[1,i,t,4] <- 0 #prob a turtle that arrived at Lalo will leave (zero b/c they didn't nest yet)
       
       ps[2,i,t,1] <- 0 #prob a turtle that nested will arrive at Lalo (0 b/c they already arrived and nested)
-      ps[2,i,t,2] <- 0 #prob a turtle that nested will nest (0 b/c we know they take a break between clutches)
-      ps[2,i,t,3] <- phi[t] #prob a turtle that nested will begin an internesting interval (if they survive they will have an internesting interval then nest again)
+      ps[2,i,t,2] <- phi[t]*0.5 #prob a turtle that nested will nest (0 b/c we know they take a break between clutches)
+      ps[2,i,t,3] <- phi[t]*0.5 #prob a turtle that nested will begin an internesting interval (if they survive they will have an internesting interval then nest again)
       ps[2,i,t,4] <- 1-phi[t] #prob a turtle that nested will leave Lalo (if they "die" then they leave)
       
       ps[3,i,t,1] <- 0 #prob a turtle that completed an internesting interval will arrive at Lalo (0 b/c they already arrived and nested)
-      ps[3,i,t,2] <- 1 #prob a turtle that completed an internesting interval will nest (1 b/c that will always happen)
-      ps[3,i,t,3] <- 0 #prob a turtle that completed an internesting interval will have another internesting interval (0 b/c they just took a break)
+      ps[3,i,t,2] <- 0.5 #prob a turtle that completed an internesting interval will nest (1 b/c that will always happen)
+      ps[3,i,t,3] <- 0.5 #prob a turtle that completed an internesting interval will have another internesting interval (0 b/c they just took a break)
       ps[3,i,t,4] <- 0 #prob at turtle that completed an internesting interval will leave Lalo (0 b/c they can only leave after nesting)
       
       ps[4,i,t,1] <- 0 #prob a turtle that left lalo will arrive at Lalo (0 b/c they already arrived, nested, and left)
       ps[4,i,t,2] <- 0 #prob a turtle that left lalo will nest (0 b/c they left)
       ps[4,i,t,3] <- 0 #prob a turtle that left lalo will have another internesting interval (0 b/c they left)
-      ps[4,i,t,4] <- 1 #prob at turtle that left lalo will leave Lalo (1 b/c they left and are gone for the resst of season)
+      ps[4,i,t,4] <- 1 #prob at turtle that left lalo will leave Lalo (1 b/c they left and are gone for the rest of season)
       
       #Define probabilities of O(t) given S(t)
-      po[1,i,t,1] <- 0 #prob a turtle that arrived to Lalo is seen (0 b/c it has not nested so no opportunity to see it on survey)
-      po[1,i,t,2] <- 1 #prob a turtle that arrived to Lalo is not seen (1 b/c it has not nested so no opportunity to see it on survey)
+      po[1,i,t,1] <- 0 #prob a turtle that arrived to Lalo is seen (0 b/c it will not be seen on a night survey)
+      po[1,i,t,2] <- 1 #prob a turtle that arrived to Lalo is not seen (1 b/c it will not be seen on a night survey)
       
       po[2,i,t,1] <- p[t] #prob a turtle that is nesting is seen (equal to capture probability)
       po[2,i,t,2] <- 1-p[t] #prob a turtle that is nesting is not seen (equal to the inverse of the capture probability)
@@ -134,63 +134,49 @@ honu <- as.matrix(honu)
 nz <- 500 #number of rows we want our observation matrix to be augmented
 
 #Add dummy occasion
-CH.du <- cbind(rep(0, dim(honu)[1]), honu) # add extra col at start so that honu are not seen on first day
+CH.du <- cbind(rep(0, dim(honu)[1]), honu) #add extra col at start so that honu are not seen on first day
+CH.du #look at edited matrix
 
-#Augment the data
-CH.du <- rbind(CH.du, matrix(0, ncol = dim(CH.du)[2], nrow = nz))
-
-CH.du #look at edited df
-
-#Recode CH matrix: a 0 is not allowed in WinBUGS!
+#Create Capture History Matrix to Pass to jags.data
 CH.ms <- CH.du
-CH.ms[CH.ms==0] <- 2 #Not seen = 2, seen = 1
-CH.ms  #final capture history matrix to be passed to data
+CH.ms[CH.ms==0] <- 2 #Zeros are not allowed. Now: 1= Seen, 2= Not Seen
+CH.ms  #look at intermediate capture history matrix. This will be used to create z matrices NOT passed to jags.data
+CH.ms.fin <- my.z <- rbind(CH.ms, matrix(2, ncol = dim(CH.ms)[2], nrow = nz)) #Augment the intermediate matrix. This is the final capture history matrix that will be passed to jags.data
 
-known.state.ms <- CH.ms #create final known state z matrix to pass to data
-known.state.ms[known.state.ms==2] <- NA #correctly identify all other instances
-known.state.ms
 
-my.z.init.ms <- CH.ms #create initial z matrix to pass to init
-my.z.init.ms[my.z.init.ms==1] <- NA #correctly identify all other instances
-my.z.init.ms
+# Code from: https://groups.google.com/g/hmecology/c/S4HO-tnzep8?pli=1 START
+#my.z is a intermediate matrix that will be manipulated to create the known state and initial z matrices 
+my.z <- CH.ms #create z matrix out of new df with added 0 at the start
 
-#NOT USING RN: START -------------------------------------------------------------
-# Code to fix error from: https://groups.google.com/g/hmecology/c/S4HO-tnzep8?pli=1 START
-#my.z.init <- CH.du #create z matrix out of new df with added 0 at the start
+first.one <- apply(my.z[,1:ncol(CH.ms)], 1, function(x) min(which(x == 1))) #identify the first nesting occurrence for each honu
+last.one  <- apply(my.z[,1:ncol(CH.ms)], 1, function(x) max(which(x == 1))) #identify the last nesting occurrence for each honu
 
-#first.one <- apply(my.z.init[,1:ncol(CH.du)], 1, function(x) min(which(x == 1))) #identify the first nesting occurrence for each honu
-#last.one  <- apply(my.z.init[,1:ncol(CH.du)], 1, function(x) max(which(x == 1))) #identify the last nesting occurrence for each honu
+for(i in 1:nrow(my.z)) { #create function to edit each row to reflect true start and end nesting periods for each honu
+  if(first.one[i] > 1)               my.z[i,                1  : (first.one[i] - 1) ] = 5 #make all values before the first nest (Pre Lalo)= 5 (to be changed later to 1)
+  if(last.one[i]  < ncol(my.z)) my.z[i, (last.one[i] + 1) : ncol(my.z)    ] = 4 #make all values after the last nest (correctly identify all post Lalo instances)
+} #at this point: 5= Pre Lalo, 1= Nesting, 0= Internesting, and 4= Post Lalo. Below we will fix this to accurately convey the states
 
-#for(i in 1:nrow(my.z.init)) { #create function to edit each row to reflect true start and end nesting periods for each honu
-  #if(first.one[i] > 1)               my.z.init[i,                1  : (first.one[i] - 1) ] = 5 #make all values before the first nest= 4 (to be changed later...)
-  #if(last.one[i]  < ncol(my.z.init)) my.z.init[i, (last.one[i] + 1) : ncol(my.z.init)    ] = 4 #make all values after the last nest (correctly identify all post Lalo instances)
-#} #at this point: 5= Pre Lalo, 1= Nesting, 0= Internesting, and 4= Post Lalo
+my.z[my.z==2] <- 3 #correctly identify all internesting instances
+my.z[my.z==1] <- 2 #correctly identify all nesting instances 
+my.z[my.z==5] <- 1 #correctly identify all pre Lalo instances
+# Code from: https://groups.google.com/g/hmecology/c/S4HO-tnzep8?pli=1 END
 
-#my.z.init.ms <- rbind(my.z.init, matrix(0, ncol = dim(my.z.init)[2], nrow = nz)) #create final z matrix to pass to init
-#my.z.init.ms[my.z.init.ms==1] <- NA #correctly identify all nesting instances 
-#my.z.init.ms[my.z.init.ms==0] <- 3 #correctly identify all internesting instances
-#my.z.init.ms[my.z.init.ms==5] <- 1 #correctly identify all pre Lalo instances
-#my.z.init.ms #look at z matrix. Now: 1= Pre Lalo, NA= Nesting, 3= Internesting, 4= Post Lalo
-# Code to fix error from: https://groups.google.com/g/hmecology/c/S4HO-tnzep8?pli=1 END
+#Augment the intermediate z matrix
+my.z <- rbind(my.z, matrix(1, ncol = dim(my.z)[2], nrow = nz)) #add more rows of NAs
+my.z #look at z matrix. Now: 1= Pre Lalo, 2= Nesting, 3= Internesting, 4= Post Lalo
 
-#Augment the data
-#CH.du <- rbind(CH.du, matrix(0, ncol = dim(CH.du)[2], nrow = nz))
+#Create Known State Z Matrix to Pass to jags.data
+known.state.ms <- my.z #create final known state z matrix to pass to data
+known.state.ms[known.state.ms!=2] <- NA #correctly identify all other instances
+known.state.ms #look at known state z matrix. Now: NA= Pre Lalo, 2= Nesting, NA= Internesting, NA= Post Lalo
 
-#known.state.ms <- CH.du #create final z matrix to pass to data
-#known.state.ms[known.state.ms==1] <- 2 #correctly identify all nesting instances 
-#known.state.ms[known.state.ms==0] <- NA #correctly identify all other instances
-#known.state.ms
-
-#CH.ms <- rbind(CH.du, matrix(0, ncol = dim(CH.du)[2], nrow = nz)) #create observation matrix
-
-#Recode CH matrix: a 0 is not allowed in WinBUGS!
-#CH.ms <- CH.du
-#CH.ms[CH.ms==0] <- 2 #Not seen = 2, seen = 1
-#CH.ms  #final capture history matrix to be passed to data
-#NOT USING RN: END -------------------------------------------------------------
+#Create Initial Z Matrix to Pass to inits
+my.z.init.ms <- my.z #create initial z matrix to pass to init
+my.z.init.ms[my.z.init.ms==2] <- NA #correctly identify all other instances
+my.z.init.ms#look at initial z matrix. Now: 1= Pre Lalo, NA= Nesting, 3= Internesting, 4= Post Lalo
 
 #Bundle data
-jags.data <- list(y = CH.ms, n.occasions = dim(CH.ms)[2], M = dim(CH.ms)[1], z= known.state.ms) 
+jags.data <- list(y = CH.ms.fin, n.occasions = dim(CH.ms.fin)[2], M = dim(CH.ms.fin)[1], z= known.state.ms) 
 
 inits <- function(){list(mean.phi = runif(1, 0, 1), 
                          mean.p = runif(1, 0, 1),
@@ -199,7 +185,7 @@ inits <- function(){list(mean.phi = runif(1, 0, 1),
 #Parameters monitored
 params <- c("mean.p", "mean.phi", "b", "Nsuper", "N", "B")
 
-# MCMC settings
+#MCMC settings
 ni <- 20000 #number of interactions
 nt <- 3 #thinning rate
 nb <- 5000 #burn-in length
